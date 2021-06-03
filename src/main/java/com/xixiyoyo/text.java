e280bb49c8dbfcd02a870ec961af47ad38bcbc70
xixiyoyo <481416026@qq.com>
2021-06-03 13:44:37 +0800

    second commit

--- src/main/java/com/xixiyoyo/text.java
+++ src/main/java/com/xixiyoyo/text.java
@@ -3,5 +3,9 @@ package com.xixiyoyo;
 public class text {
   public static void main(String[] args) {
     System.out.println("hello git!");
+    System.out.println("hello git2!");
+    System.out.println("hello git2!");
+
+
   }
 }
